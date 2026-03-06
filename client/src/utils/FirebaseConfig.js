d236f6ae80edda5d831918f881348f4b889d23e3
fathima <mizbafathima110@gmail.com>
2026-03-06 19:55:44 +0530

    fixed

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,4 +1,3 @@
-// client/src/utils/FirebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -14,6 +13,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
-
-// Enable CORS for cross-origin requests
-firebaseAuth.useDeviceLanguage();
